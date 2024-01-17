a85654cde256095e194154195ef6553e79446005
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2024-01-17 16:57:14 +0000

    retrieve pokemon cards from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [pokeData, setPokeData] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		let pageNum = 11;
+
+		fetch(
+			`https://api.pokemontcg.io/v2/cards?select=id,name,images&page=${pageNum}`
+		)
+			.then(function (response) {
+				return response.json();
+			})
+
+			.then(function (response) {
+				const data = response.data;
+				console.log(data);
+
+				let filtered = data.filter((item) => item.id.includes("base"));
+				console.log(filtered);
+				setPokeData([...pokeData, ...filtered]);
+			})
+
+			.catch(function (err) {
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<>
+			{pokeData.map((item) => {
+				return <img key={item.id} src={item.images.small} alt="" />;
+			})}
+		</>
+	);
 }
 
-export default App
+export default App;
