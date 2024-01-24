a6241df1589b60f2788b14191cb110b15123220d
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2024-01-24 16:57:42 +0000

    detect if card already picked and increase score

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { StrictMode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import getData from "./components/getData.js";
 import CardRenderer from "./components/CardRenderer";
 import "./App.css";
 
 function App() {
 	const [pokeData, setPokeData] = useState([]);
-
+	const [score, setScore] = useState(0);
 	useEffect(() => {
 		(async () => {
 			setPokeData(await getData());
 		})();
 	}, []);
 
-	return pokeData && <CardRenderer cards={pokeData} />;
+	return (
+		<>
+			{pokeData.length > 0 ? (
+				<>
+					{" "}
+					<h1>Score:{score}</h1>{" "}
+					<CardRenderer setScore={() => setScore(score + 1)} cards={pokeData} />
+				</>
+			) : (
+				<h1>Loading...</h1>
+			)}
+		</>
+	);
 }
 export default App;
--- src/components/CardRenderer.jsx
+++ src/components/CardRenderer.jsx
@@ -1,12 +1,23 @@
 import { useEffect, useState } from "react";
-
-export default function CardRenderer({ cards }) {
+let selectedCards = [];
+export default function CardRenderer({ setScore, cards }) {
 	const [randomCards, setRandomCards] = useState([]);
 
-	function generateNumbers() {
+	function checkCards(cardId) {
+		if (selectedCards.find((card) => card.id === cardId) !== undefined) {
+			console.log("CARD ALREADY CHOSEN");
+		} else {
+			selectedCards.push(
+				randomCards[randomCards.findIndex((item) => item.id === cardId)]
+			);
+			setScore();
+		}
+	}
+
+	function shuffleCards() {
 		let rands = [];
 		while (rands.length < 10) {
-			let r = Math.floor(Math.random() * 10);
+			let r = Math.floor(Math.random() * 15);
 			if (rands.indexOf(r) === -1) {
 				rands.push(r);
 			}
@@ -20,7 +31,7 @@ export default function CardRenderer({ cards }) {
 	}
 
 	useEffect(() => {
-		generateNumbers();
+		shuffleCards();
 	}, [cards]);
 
 	return (
@@ -29,8 +40,12 @@ export default function CardRenderer({ cards }) {
 				randomCards.map((item) => {
 					return (
 						<img
-							onClick={generateNumbers}
+							onClick={(e) => {
+								checkCards(e.target.getAttribute("data"));
+								shuffleCards();
+							}}
 							key={item.id}
+							data={item.id}
 							src={item.images.small}
 							alt=""
 						/>
